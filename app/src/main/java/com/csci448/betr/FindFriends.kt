3f6c250039bf1751a407b0e8b0c7a0f19e8f35f8
jaredparrish <jaredparrish@mymail.mines.edu>
2019-03-11 18:11:20 -0600

    added temp button to move from friends list to create bet page

--- app/src/main/java/com/csci448/betr/FindFriends.kt
+++ app/src/main/java/com/csci448/betr/FindFriends.kt
@@ -1,13 +1,21 @@
 package com.csci448.betr
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.find_friends.*
 
 class FindFriends: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.find_friends)
+        tempButton.setOnClickListener{
+            val intent = Intent(this, CreateBet::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
