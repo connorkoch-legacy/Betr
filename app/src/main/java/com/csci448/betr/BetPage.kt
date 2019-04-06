092211130bce1f8e773edd14579d9be270bbeb32
Jessy Liao <jessyliao@mines.edu>
2019-04-05 20:55:25 -0600

    added recyclerview implementation for BetPage

--- app/src/main/java/com/csci448/betr/BetPage.kt
+++ app/src/main/java/com/csci448/betr/BetPage.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_bet_page.*
 
 class BetPage : AppCompatActivity() {
@@ -18,6 +19,26 @@ class BetPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bet_page)
+
+        val tempUser = User("Test User", "pass", null, null)
+        val tempFriend = User("Friend 1", "pass1", null, null)
+        val tempFriend2 = User("Friend 2", "pass2", null, null)
+        var userFriendList = mutableListOf<User>(tempFriend, tempFriend2)
+        var friendFriendList = mutableListOf<User>(tempFriend2)
+        tempUser.friendList = userFriendList
+        tempFriend.friendList = friendFriendList
+
+        var userBet = Bet("I bet that the moon is cheese", tempUser, tempFriend, 100.00)
+        var friendBet = Bet("I bet that dogs are bigger than cats", tempFriend, tempFriend2, 17.8932)
+        var userBetList = mutableListOf<Bet>(userBet, friendBet)
+        var friendBetList = mutableListOf<Bet>(friendBet)
+        tempUser.betList = userBetList
+        tempFriend.betList = userBetList
+        tempFriend2.betList = friendBetList
+
+        betpage_recyclerview.layoutManager = LinearLayoutManager(this)
+        betpage_recyclerview.adapter = BetPageAdapter(tempUser)
+
 
         temp_finished_bet_button.setOnClickListener {
             var intent = Intent(this, FinishedBet::class.java)
