ac6ae7e7d1d3224743356d2ff9772aee3ef951b8
connor <connorkoch@mymail.mines.edu>
2019-04-05 20:45:24 -0600

    shit

--- app/src/main/java/com/csci448/betr/CreateAccount.kt
+++ app/src/main/java/com/csci448/betr/CreateAccount.kt
@@ -3,11 +3,29 @@ package com.csci448.betr
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.create_account.*
 
 class CreateAccount: AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_account)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        create_button.setOnClickListener {
+            //REGEX to check for correct email type, passwords are the same
+            
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null)
+        val currentUser = auth.currentUser
     }
 }
--- app/src/main/java/com/csci448/betr/LoginPage.kt
+++ app/src/main/java/com/csci448/betr/LoginPage.kt
@@ -3,12 +3,21 @@ package com.csci448.betr
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login_page.*
 
 class LoginPage : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
         setContentView(R.layout.activity_login_page)
 
         sign_up_button.setOnClickListener {
@@ -17,9 +26,33 @@ class LoginPage : AppCompatActivity() {
         }
 
         login_button.setOnClickListener {
-            var intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+//            var intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+
+            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, create correct User object and move to main activity
+
+
+                        var intent = MainActivity.newIntent(this)
+                        //intent.putExtra(User)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
         }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
     }
 
 }
